93d243f4d1e86dc028cb95189ef60de57b7da426
Roy199898 <dipr19989@gmail.com>
2021-02-08 11:33:56 +0600

    lab01 done

---
+++ lab01/Registration.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>My Exercise</title>
+</head>
+<body>
+	<h1 style="color: blue;
+	" >REGISTRATION</h
+	
+
+	
+	<br><br>
+	<form>
+		<table>
+			<tr>
+		<td>Name:</td>
+		<td><input type=" text" id="1" name="A" placeholder="Enter your name"></td>
+	</tr>
+	<tr>
+		<td>Email:</td>
+		<td><input type=" text" id="2" name="B"></td></tr>
+		<tr><td>User Name:</td>
+		<td><input type=" text" id="3" name="C"></td></tr>
+		<tr><td>Password:</td>
+		
+		<td><input type=" text" id="4" name="D"></td></tr>
+		<tr><td> Confirm Password:</td>
+			<td><input type="password" id="e" name="email"></td></tr>
+			<tr>
+			<td><input type="radio" id="male" name="gender" value="male"></td></tr>
+           <label for="male">Male</label><br>
+           <td><input type="radio" id="female" name="gender" value="female"></td></tr>
+           <label for="female">Female</label><br>
+           <input type="radio" id="other" name="gender" value="other">
+           <label for="other">Other</label>
+
+		<td><input type="submit" value="SUBMIT"><td>
+			<td><input type="reset" value="Reset"></tr>
+	</form>
+</table>
+
+
+
+</body>
+</html> 
